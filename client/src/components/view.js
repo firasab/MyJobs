0fb1041f0c9ee41257ffdb20a5b7bc0a0ee33194
firas <firas.abu.sneneh@hotmail.com>
2021-12-31 06:45:35 +0200

    add alerts

--- client/src/components/view.js
+++ client/src/components/view.js
@@ -11,9 +11,8 @@ const view = () => {
         <div >
             <Container maxWidth="lg">
                 <AppBar className={classes.appBar} position="static" color="inherit">
-                    <Typography className={classes.heading} variant="h2" align="center"> Job view </Typography>
+                    <Typography className={classes.heading} variant="h2" align="center"> Welcome to MyJobs website!</Typography>
                 </AppBar>
-
                 <Grow in>
                     <Container>
                             <Grid item xs={12} sm={12}>

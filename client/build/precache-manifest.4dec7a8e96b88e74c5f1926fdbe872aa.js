c74e2173e4f76259c3637e341facc692e2d35773
firas <firas.abu.sneneh@hotmail.com>
2022-03-03 11:45:54 +0200

    deployment

---
+++ client/build/precache-manifest.4dec7a8e96b88e74c5f1926fdbe872aa.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1261d03de1dd09699670e765620da02",
+    "url": "/ManageWorkers/MyJobs/build/index.html"
+  },
+  {
+    "revision": "5d6c8f32cc6aa56bc30c",
+    "url": "/ManageWorkers/MyJobs/build/static/css/2.7bfada36.chunk.css"
+  },
+  {
+    "revision": "133185e8d7972ec2204b",
+    "url": "/ManageWorkers/MyJobs/build/static/css/main.bf57e93a.chunk.css"
+  },
+  {
+    "revision": "5d6c8f32cc6aa56bc30c",
+    "url": "/ManageWorkers/MyJobs/build/static/js/2.93dda1bc.chunk.js"
+  },
+  {
+    "revision": "772a53d19cac9f6524a08fc6e175093c",
+    "url": "/ManageWorkers/MyJobs/build/static/js/2.93dda1bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "133185e8d7972ec2204b",
+    "url": "/ManageWorkers/MyJobs/build/static/js/main.451d597e.chunk.js"
+  },
+  {
+    "revision": "29441067fedcee9dc770",
+    "url": "/ManageWorkers/MyJobs/build/static/js/runtime-main.91bc1696.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/ManageWorkers/MyJobs/build/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/ManageWorkers/MyJobs/build/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/ManageWorkers/MyJobs/build/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/ManageWorkers/MyJobs/build/static/media/notification.c0d3c94c.eot"
+  }
+]);

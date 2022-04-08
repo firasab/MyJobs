efc7fc157dd7803ff54e381e62b2c4e45f7dbcc4
firas <firas.abu.sneneh@hotmail.com>
2022-04-08 22:10:53 +0300

    comments

---
+++ mobile_app/src/components/common/EditButton/EditButton.js
@@ -0,0 +1,39 @@
+import { View, Text, StyleSheet, Pressable } from "react-native";
+import React from "react";
+
+
+const EditButton = ({ onPress, text, type }) => {
+  return (
+    <Pressable
+      onPress={onPress}
+      style={[styles.container, styles[`container_${type}`]]}
+    >
+      <Text style={[styles.text, styles[`text_${type}`]]}>{text}</Text>
+    </Pressable>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    padding: 15,
+    marginVertical: 10,
+    alignItems: "center",
+    borderRadius: 5,
+  },
+  container_PRIMARY: { backgroundColor: "#3B71F3" },
+  container_SECONDARY: { backgroundColor: "orange" },
+  container_TERTIARY: { backgroundColor: "white" },
+  container_FORTH: { backgroundColor: "#00BFFF" },
+  text: {
+    fontWeight: "bold",
+  },
+  text_PRIMARY: {
+    color: "white",
+  },
+  text_SECONDARY: { color: "white" },
+  text_TERTIARY: {
+    color: "gray",
+  },
+});
+
+export default EditButton;
